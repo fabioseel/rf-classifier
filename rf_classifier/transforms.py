51c085624b9c1b12a41a18d4da0a6f7d4ecc125a
Fabio Seel <fabio.seel@uni-tuebingen.de>
2024-03-01 17:23:25 +0100

    feat: add random color rescale transform

--- rf_classifier/transforms.py
+++ rf_classifier/transforms.py
@@ -47,3 +47,24 @@ class RandomAffine(torch.nn.Module):
         inp_avg = img.mean(axis=(1,2))
         transf = transforms.RandomAffine(**self.affine_args, fill=[*inp_avg])
         return transf(img)
+    
+class RandomColorRescale(torch.nn.Module):
+    def __init__(self, r=(0,1), g=(0,1), b=(0,1)):
+        super().__init__()
+        self.r = r
+        self.g = g
+        self.b = b
+
+    def forward(self, img):
+        """
+        Args:
+            img (PIL Image or Tensor): Image to be flipped.
+
+        Returns:
+            Image with rescaled color channels.
+        """
+        rand = torch.rand(3)
+        rand[0] = rand[0]*(self.r[1]-self.r[0])+self.r[0]
+        rand[1] = rand[1]*(self.g[1]-self.g[0])+self.g[0]
+        rand[2] = rand[2]*(self.b[1]-self.b[0])+self.b[0]
+        return img*rand[:,None,None]
