6e18ebd55bc80b88f302c48fc6d1127addc09b5f
Fabio Seel <fabio.seel@uni-tuebingen.de>
2024-03-06 15:23:41 +0100

    change: NearestInterpolation used, added gaussian, changed unclassifiable

--- rf_classifier/datasets.py
+++ rf_classifier/datasets.py
@@ -2,12 +2,12 @@ from torch.utils.data import Dataset
 from rf_classifier import rf_generator
 from random import uniform as urand, randint
 import torch
-from torchvision.transforms import Resize
+from torchvision.transforms import Resize, InterpolationMode
 import numpy as np
 
 
 class SyntheticRFsDataset(Dataset):
-    def __init__(self, num_samples_per_class, transform = None):
+    def __init__(self, num_samples_per_class, transform=None):
         self.num_samples_per_class = num_samples_per_class
         self.num_classes = 7
         self.classes = [
@@ -31,14 +31,17 @@ class SyntheticRFsDataset(Dataset):
                 # Generate image for each class
                 image = self.generate_image(class_idx)
                 # Convert image to tensor
-                image_tensor = Resize((32,32), antialias=True)(torch.from_numpy(image).to(dtype=torch.float32).movedim(2,0))
+                image_tensor = Resize(
+                    (32, 32), antialias=True, interpolation=InterpolationMode.NEAREST
+                )(torch.from_numpy(image).to(dtype=torch.float32).movedim(2, 0))
                 # Append image and label to data list
                 data.append((image_tensor, class_idx))
         return data
 
-    def generate_image(self, class_idx, image_size = None):
+    def generate_image(self, class_idx, image_size=None):
         if image_size is None:
-            image_size = [randint(7,64)]*2
+            image_size = [randint(7, 64)] * 2
+
         if class_idx == 0:
             sigma1 = urand(1, image_size[0] / 2)
             sigma2 = urand(1, image_size[1] / 2)
@@ -62,12 +65,14 @@ class SyntheticRFsDataset(Dataset):
                 shape=image_size, color=(urand(-1, 1), urand(-1, 1), urand(-1, 1))
             )
         elif class_idx == 2:
+
+            sigma_x = urand(image_size[0] / 8, image_size[0] * 3 / 4)
             rf = rf_generator.gabor_kernel(
                 shape=image_size,
                 frequency=1 / urand(image_size[0] / 8, image_size[0] / 2),
                 theta=urand(0, torch.pi),
-                sigma_x=urand(2, image_size[0] / 2),
-                sigma_y=urand(2, image_size[1] / 2),
+                sigma_x=sigma_x,
+                sigma_y=urand(sigma_x * 0.5, sigma_x * 1.5),
                 center_offset=(
                     urand(-image_size[0] / 4, image_size[0] / 4),
                     urand(-image_size[1] / 4, image_size[1] / 4),
@@ -90,21 +95,29 @@ class SyntheticRFsDataset(Dataset):
         elif class_idx == 4:
             rf = rf_generator.decorrelated_color_noise(shape=image_size, low=-1)
         elif class_idx == 5:
-            rf = rf_generator.simple_edge(
-                shape=image_size, theta=urand(0, torch.pi)
-            )
+            rf = rf_generator.simple_edge(shape=image_size, theta=urand(0, torch.pi))
             rf = rf_generator.greyscale_to_color(
                 rf, urand(-1, 1), urand(-1, 1), urand(-1, 1)
             )
         elif class_idx == 6:  # Unclassifiable. Need better generation here...
             rf = (
                 self.generate_image(0, image_size) * self.generate_image(1, image_size)
-                + self.generate_image(2, image_size) * self.generate_image(3, image_size)
-                + self.generate_image(4, image_size) * self.generate_image(5, image_size)
+                + self.generate_image(2, image_size)
+                * self.generate_image(3, image_size)
+                + self.generate_image(4, image_size)
+                * self.generate_image(5, image_size)
             )
 
-        return rf + rf_generator.decorrelated_color_noise(
+        rf += rf_generator.decorrelated_color_noise(
             shape=image_size, low=-0.02, high=0.02
+        )
+
+        gauss_sigma_x = urand(image_size[0] / 8, image_size[0] * 3 / 4)
+        return rf_generator.apply_gaussian(
+            rf,
+            theta=urand(0, torch.pi),
+            sigma_x=gauss_sigma_x,
+            sigma_y=urand(gauss_sigma_x * 0.5, gauss_sigma_x * 1.5),
         )
 
     def __len__(self):
--- rf_classifier/rf_generator.py
+++ rf_classifier/rf_generator.py
@@ -1,6 +1,38 @@
 import numpy as np
 
-def gabor_kernel(shape=(16, 16), frequency=1/8, theta=np.pi/4, sigma_x=3, sigma_y=3, phase_offset=0, center_offset=(0, 0), factor=1, offset=0):
+
+def gaussian_kernel(shape=(16, 16), theta=0, sigma_x=3, sigma_y=3):
+    x, y = np.meshgrid(
+        np.linspace(-shape[1] / 2, shape[1] / 2, shape[1]),
+        np.linspace(-shape[0] / 2, shape[0] / 2, shape[0]),
+    )
+    x_theta = x * np.cos(theta) + y * np.sin(theta)
+    y_theta = -x * np.sin(theta) + y * np.cos(theta)
+    return np.exp(-0.5 * (x_theta**2 / sigma_x**2 + y_theta**2 / sigma_y**2))
+
+
+def apply_gaussian(img, theta=0, sigma_x=None, sigma_y=None):
+    if sigma_x is not None:
+        if sigma_y is None:
+            sigma_y = sigma_x
+        envelope = gaussian_kernel(img.shape, theta, sigma_x, sigma_y)
+        if len(img.shape) == 3:
+            envelope = envelope[..., None]
+        img *= envelope
+    return img
+
+
+def gabor_kernel(
+    shape=(16, 16),
+    frequency=1 / 8,
+    theta=np.pi / 4,
+    sigma_x=3,
+    sigma_y=3,
+    phase_offset=0,
+    center_offset=(0, 0),
+    factor=1,
+    offset=0,
+):
     """
     Generate a Gabor kernel with the specified shape, center offset, and phase offset.
 
@@ -18,24 +50,38 @@ def gabor_kernel(shape=(16, 16), frequency=1/8, theta=np.pi/4, sigma_x=3, sigma_
     Returns:
         numpy.ndarray: Gabor kernel with the specified shape, center offset, and phase offset.
     """
-    x, y = np.meshgrid(np.linspace(-shape[1]/2, shape[1]/2, shape[1]), np.linspace(-shape[0]/2, shape[0]/2, shape[0]))
-    
+    x, y = np.meshgrid(
+        np.linspace(-shape[1] / 2, shape[1] / 2, shape[1]),
+        np.linspace(-shape[0] / 2, shape[0] / 2, shape[0]),
+    )
+
     # Apply center offset to the coordinates
     x -= center_offset[0]
     y -= center_offset[1]
-    
+
     x_theta = x * np.cos(theta) + y * np.sin(theta)
     y_theta = -x * np.sin(theta) + y * np.cos(theta)
-    
+
     envelope = np.exp(-0.5 * (x_theta**2 / sigma_x**2 + y_theta**2 / sigma_y**2))
     sinusoid = np.cos(2 * np.pi * frequency * x_theta + phase_offset)
-    
+
     gabor_kernel = envelope * sinusoid
     return gabor_kernel * factor + offset
 
-def center_surround(shape=(16,16), theta =np.pi/4, sigma_x1=1, sigma_y1=1, sigma_x2=2, sigma_y2=2, center_offset=(0, 0), factor=1, offset=0):
+
+def center_surround(
+    shape=(16, 16),
+    theta=np.pi / 4,
+    sigma_x1=1,
+    sigma_y1=1,
+    sigma_x2=2,
+    sigma_y2=2,
+    center_offset=(0, 0),
+    factor=1,
+    offset=0,
+):
     """
-    Generate a Gabor kernel with the specified shape, center offset, and phase offset.
+    Generate a center surround RF with the specified shape, center offset, and phase offset.
 
     Parameters:
         shape (tuple, optional): Desired output shape (height, width) of the center surround rf. Default is (16, 16).
@@ -52,20 +98,26 @@ def center_surround(shape=(16,16), theta =np.pi/4, sigma_x1=1, sigma_y1=1, sigma
         numpy.ndarray: Gabor kernel with the specified shape, center offset, and phase offset.
     """
 
-    x, y = np.meshgrid(np.linspace(-shape[1]/2, shape[1]/2, shape[1]), np.linspace(-shape[0]/2, shape[0]/2, shape[0]))
-    
+    x, y = np.meshgrid(
+        np.linspace(-shape[1] / 2, shape[1] / 2, shape[1]),
+        np.linspace(-shape[0] / 2, shape[0] / 2, shape[0]),
+    )
+
     # Apply center offset to the coordinates
     x -= center_offset[0]
     y -= center_offset[1]
-    
+
     x_theta = x * np.cos(theta) + y * np.sin(theta)
     y_theta = -x * np.sin(theta) + y * np.cos(theta)
 
     gaussian1 = np.exp(-0.5 * (x_theta**2 / sigma_x1**2 + y_theta**2 / sigma_y1**2))
     gaussian2 = np.exp(-0.5 * (x_theta**2 / sigma_x2**2 + y_theta**2 / sigma_y2**2))
-    return (sigma_x2/sigma_x1*gaussian1-gaussian2)*factor + offset
+    return (sigma_x2 / sigma_x1 * gaussian1 - gaussian2) * factor + offset
 
-def noise(shape=(16,16), low=0, high=1):
+
+def noise(
+    shape=(16, 16), low=0, high=1, gauss_theta=0, gauss_sigma_x=None, gauss_sigma_y=None
+):
     """
     Generate random noise with uniform distribution.
 
@@ -77,9 +129,13 @@ def noise(shape=(16,16), low=0, high=1):
     Returns:
         numpy.ndarray: A 2D array of random noise with shape `shape`.
     """
-    return np.random.uniform(low=low, high=high,size=shape)
+    _noise = np.random.uniform(low=low, high=high, size=shape)
+    return _noise
 
-def decorrelated_color_noise(shape=(16,16), low=0, high=1):
+
+def decorrelated_color_noise(
+    shape=(16, 16), low=0, high=1, gauss_theta=0, gauss_sigma_x=None, gauss_sigma_y=None
+):
     """
     Generate decorrelated color noise.
 
@@ -91,9 +147,13 @@ def decorrelated_color_noise(shape=(16,16), low=0, high=1):
     Returns:
         numpy.ndarray: A 3D array of decorrelated color noise with shape `(shape[0], shape[1], 3)`.
     """
-    return np.stack([noise(shape, low, high),noise(shape, low, high),noise(shape, low, high)], axis=2)
+    return np.stack(
+        [noise(shape, low, high), noise(shape, low, high), noise(shape, low, high)],
+        axis=2,
+    )
 
-def greyscale_to_color(greyscale_input, r_factor=1,g_factor=1,b_factor=1):
+
+def greyscale_to_color(greyscale_input, r_factor=1, g_factor=1, b_factor=1):
     """
     Convert a greyscale image to a color image by duplicating the input channel and scaling it according to the factors.
 
@@ -106,9 +166,17 @@ def greyscale_to_color(greyscale_input, r_factor=1,g_factor=1,b_factor=1):
     Returns:
         numpy.ndarray: A 3D array representing the color image with shape `(greyscale_input.shape[0], greyscale_input.shape[1], 3)`.
     """
-    return np.stack([greyscale_input*r_factor,greyscale_input*g_factor,greyscale_input*b_factor], axis=2)
+    return np.stack(
+        [
+            greyscale_input * r_factor,
+            greyscale_input * g_factor,
+            greyscale_input * b_factor,
+        ],
+        axis=2,
+    )
 
-def color(shape=(16,16), color=(1,0,0)):
+
+def color(shape=(16, 16), color=(1, 0, 0)):
     """
     Generate an image of given shape, filled with the defined color.
 
@@ -119,11 +187,22 @@ def color(shape=(16,16), color=(1,0,0)):
     Returns:
         numpy.ndarray: Image with shape (h, w,3).
     """
-    return np.stack([np.full(shape, fill_value=color[0]),np.full(shape, fill_value=color[1]),np.full(shape, fill_value=color[2])], axis=2)
+    return np.stack(
+        [
+            np.full(shape, fill_value=color[0]),
+            np.full(shape, fill_value=color[1]),
+            np.full(shape, fill_value=color[2]),
+        ],
+        axis=2,
+    )
 
-def mult_freq(shape=(16,16), freq1=1/4, freq2=1/2, theta1=0, theta2=np.pi/2):
-    x, y = np.meshgrid(np.linspace(-shape[1]/2, shape[1]/2, shape[1]), np.linspace(-shape[0]/2, shape[0]/2, shape[0]))
-    
+
+def mult_freq(shape=(16, 16), freq1=1 / 4, freq2=1 / 2, theta1=0, theta2=np.pi / 2):
+    x, y = np.meshgrid(
+        np.linspace(-shape[1] / 2, shape[1] / 2, shape[1]),
+        np.linspace(-shape[0] / 2, shape[0] / 2, shape[0]),
+    )
+
     # Apply center offset to the coordinates
     x_theta1 = x * np.cos(theta1) + y * np.sin(theta1)
     sinusoid1 = np.cos(2 * np.pi * freq1 * x_theta1)
@@ -131,7 +210,10 @@ def mult_freq(shape=(16,16), freq1=1/4, freq2=1/2, theta1=0, theta2=np.pi/2):
     x_theta2 = x * np.cos(theta2) + y * np.sin(theta2)
     sinusoid2 = np.cos(2 * np.pi * freq2 * x_theta2)
 
-    return sinusoid1*sinusoid2
+    return sinusoid1 * sinusoid2
 
-def simple_edge(shape=(16,16), theta=0):
-    return gabor_kernel(shape, frequency=1/np.max(shape), theta=theta, phase_offset=np.pi/2)
+
+def simple_edge(shape=(16, 16), theta=0):
+    return gabor_kernel(
+        shape, frequency=1 / np.max(shape), theta=theta, phase_offset=np.pi / 2
+    )
